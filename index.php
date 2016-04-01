ca94735b41e7d2eaec21acea8d3d1e6290303c24
Zach <n00881958@unf.edu>
2016-04-01 12:26:31 -0400

    test

--- index.php
+++ index.php
@@ -6,3 +6,23 @@
  * Time: 10:22 AM
  */
 
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+</head>
+<body>
+<div id="header">
+    <div class="wings">
+        <div class="logo">
+            <img src="image/UNFLogo.gif"/>
+            <img src="image/UNFLogoText.gif"/>
+        </div>
+    </div>
+</div>
+<div>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id justo eu mauris mattis sollicitudin. Curabitur posuere sapien tellus, vitae sodales ligula molestie eu. Phasellus posuere orci at posuere consectetur. Suspendisse urna lorem, semper ac aliquet vel, iaculis non augue. Maecenas molestie et lectus a pretium. Nulla vitae dolor orci. Aliquam in ultricies libero, vel vehicula orci. Donec at tellus vel mauris cursus faucibus in sed elit. Proin pretium velit vitae ex dapibus suscipit. Aliquam pharetra magna quis egestas interdum. Phasellus faucibus ipsum lectus, non consectetur diam sollicitudin eu. Duis aliquet lorem a enim aliquam dignissim. Aliquam magna lacus, iaculis a sapien vel, volutpat congue nisi.</p>
+</div>
+</body>
+</html>
