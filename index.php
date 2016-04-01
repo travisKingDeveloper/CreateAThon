e0308ae2b92e7636e155d491dff54838975ee836
Zach <n00881958@unf.edu>
2016-04-01 13:23:06 -0400

    Header put in and other changes probably as well

--- index.php
+++ index.php
@@ -16,13 +16,22 @@
 <div id="header">
     <div class="wings">
         <div class="logo">
-            <img src="image/UNFLogo.gif"/>
-            <img src="image/UNFLogoText.gif"/>
+            <a href="https://www.unf.edu/"> <img src="image/UNFLogo.gif"/>
+            <img src="image/UNFLogoText.gif"/> </a>
+            <a href="https://mywings.unf.edu/"><img src="image/mywings.gif" style="float:right; padding-top: 5%"/> </a>
         </div>
     </div>
 </div>
-<div>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id justo eu mauris mattis sollicitudin. Curabitur posuere sapien tellus, vitae sodales ligula molestie eu. Phasellus posuere orci at posuere consectetur. Suspendisse urna lorem, semper ac aliquet vel, iaculis non augue. Maecenas molestie et lectus a pretium. Nulla vitae dolor orci. Aliquam in ultricies libero, vel vehicula orci. Donec at tellus vel mauris cursus faucibus in sed elit. Proin pretium velit vitae ex dapibus suscipit. Aliquam pharetra magna quis egestas interdum. Phasellus faucibus ipsum lectus, non consectetur diam sollicitudin eu. Duis aliquet lorem a enim aliquam dignissim. Aliquam magna lacus, iaculis a sapien vel, volutpat congue nisi.</p>
+<div id="links">
+    <ul>
+        <li><a href="">HOME</a></li>
+        <li><a href="">ROOM LIST</a></li>
+        <li><a href="">LOGIN</a></li>
+        <li><a href="">REGISTER</a></li>
+    </ul>
 </div>
+<!--div id="body">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id justo eu mauris mattis sollicitudin. Curabitur posuere sapien tellus, vitae sodales ligula molestie eu. Phasellus posuere orci at posuere consectetur. Suspendisse urna lorem, semper ac aliquet vel, iaculis non augue. Maecenas molestie et lectus a pretium. Nulla vitae dolor orci. Aliquam in ultricies libero, vel vehicula orci. Donec at tellus vel mauris cursus faucibus in sed elit. Proin pretium velit vitae ex dapibus suscipit. Aliquam pharetra magna quis egestas interdum. Phasellus faucibus ipsum lectus, non consectetur diam sollicitudin eu. Duis aliquet lorem a enim aliquam dignissim. Aliquam magna lacus, iaculis a sapien vel, volutpat congue nisi.</p>
+</div-->
 </body>
 </html>
